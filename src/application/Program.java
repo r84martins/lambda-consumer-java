a31624899dcf0f2c7ab2bfc4b0a7e3ce9fa87ee4
Ricardo Martins <rs84martins@gmail.com>
2020-10-14 22:56:01 -0300

    Inline lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -15,10 +14,8 @@ public class Program {
 		list.add(new Product("Mouse",50.00));
 		list.add(new Product("Tablet",350.00));
 		list.add(new Product("HD Case",80.90));
-		
-		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
-		
-		list.forEach(cons);
+				
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 		list.forEach(System.out::println);
 	}
 }
